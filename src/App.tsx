76f4788beac8d35d2dc429f94429816e43c3bc5a
thanos-pakou <thanospak@gmail.com>
2024-03-09 16:45:15 +0200

    Installed plugins for tailwind and setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import { useState } from 'react'
+
+// Add this at the top of your .tsx file
+declare var process: {
+  env: {
+    NODE_ENV: string;
+  };
+};
+
+import tw from 'twin.macro';
+import styled from 'styled-components';
+
+
+
+
 import './App.css'
+
+const StyledApp = styled.div`
+  
+
+  .my-class {
+    ${tw`text-blue-500 bg-gray-200 p-4`}
+  }
+`;
 
 function App() {
 
   return (
     <>
-
+      <StyledApp>
+        <div className='my-class'>awdwd</div>
+      </StyledApp>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import envCompatible from 'vite-plugin-env-compatible';
+import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    envCompatible(),
+    macrosPlugin(),],
 })
