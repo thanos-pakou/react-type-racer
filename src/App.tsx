074bbe6c583058cf8713f3e6c7018de0e20c0fc0
thanos-pakou <thanospak@gmail.com>
2024-03-12 14:21:28 +0200

    Integrate Tailwind CSS with Styled Components
    Add twin.macro, vite-plugin-babel-macros, and vite-plugin-env-compatible dependencies. Update vite.config.ts to add the dependencies and App.tsx to utilize them.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+declare var process: {
+  env: {
+    NODE_ENV: string;
+  };
+};
+
+import tw from 'twin.macro';
+import styled from 'styled-components';
 import './App.css'
+
+const StyledApp = styled.div`
+  
+  .my-class {
+    ${tw`text-blue-500 bg-gray-200 p-4`}
+  }
+`;
 
 function App() {
 
-
   return (
     <>
-      <div>
-        test
-      </div>
+
+      <StyledApp>
+        <div className='my-class'>Test</div>
+      </StyledApp>
     </>
   )
 }
-
-export default App
